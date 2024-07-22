b387dd5f6529be0347d1474eb66f29ff89926021
Your Name <manzi5000@hotmail.co.uk>
2024-07-22 23:00:12 +0100

    added user/password

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use("/demo", require("./routes/demo"));
+app.use("/login", require("./routes/login/login"));
 
 const port = process.env.PORT || 6001;
 app.listen(port, () => {
