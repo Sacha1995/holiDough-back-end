5413efb245075fa336f9c8d0f3167b3a876a7886
Your Name <manzi5000@hotmail.co.uk>
2024-07-24 19:35:28 +0100

    added tokens to user

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const query = require("../mySQL/connection");
 const { genToken } = require("../utils");
+const sha256 = require("sha256");
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
+  const hashed_password = sha256(process.env.SALT + password);
   const results = await query(
     `SELECT id 
       FROM users 
         WHERE email LIKE "${email}" 
           AND hashed_password 
-            LIKE "${password}";`
+            LIKE "${hashed_password}";`
   );
+  console.log(results);
   if (results.length > 0) {
-    res.send({ status: 1 });
+    const token = genToken();
+    await query(
+      `INSERT INTO tokens (user_id, token) VALUES ("${results.id}", "${token}")`
+    );
+
+    res.send({ status: 1, token });
   } else if (!email || !password) {
     res.status(400);
-    res.send({ status: 1, error: "Missing Username or Password" });
+    res.send({ status: 0, error: "Missing Username or Password" });
   } else {
     res.status(400);
     res.send({ status: 0 });
@@ -28,14 +36,19 @@ router.post("/login", async (req, res) => {
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
   try {
+    const hashed_password = sha256(process.env.SALT + password);
     const results = await query(
       `INSERT INTO users (email, hashed_password) 
-        VALUES ('${email}', '${password}')`
+        VALUES ('${email}', '${hashed_password}')`
     );
     res.send({ status: 1 });
   } catch (e) {
-    if (e.code === "ER_DUP_ENTRY") res.status(400);
-    res.send({ status: 0, error: "User already exists" });
+    if (e.code === "ER_DUP_ENTRY") {
+      res.status(400);
+      res.send({ status: 0, error: "Duplicate User" });
+    } else {
+      res.send({ status: 0, error: e });
+    }
   }
 });
 
