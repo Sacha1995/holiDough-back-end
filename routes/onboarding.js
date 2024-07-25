5785770685398b324095fcffa882afaac9d54d59
Zahra Vasanji <zahravasanji@hotmail.com>
2024-07-25 20:52:19 +0100

    added validation for onboarding route

--- routes/onboarding.js
+++ routes/onboarding.js
@@ -2,15 +2,25 @@ const express = require("express");
 const query = require("../mySQL/connection");
 const router = express.Router();
 const { addTrip } = require("../mySQL/onboardingQueries");
+const { tripSchema } = require("../validation/joi");
+const joi = require("joi");
+
 
 router.post("/", async (req, res) => {
-  const results = await query(`SHOW TABLES;`);
   const trip = req.body._onboardingDetails.details;
 
   if (!trip) {
     res.status(400).send("No trip received");
     return;
   }
+
+  const validateTrip = tripSchema.validate(trip, { abortEarly: false });
+
+  if (validateTrip.error) {
+    res.send("Trip does not match the expected format.");
+    return;
+  }
+
   try {
     const result = await query(addTrip(trip));
 
